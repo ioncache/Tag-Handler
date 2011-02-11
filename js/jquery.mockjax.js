05df8f41b0ff457edee81dee7c8bf5cb8c373353
Mark Jubenville <ioncache@gmail.com>
2011-02-10 19:17:19 -0500

    Updated the options table to show new options.
    Updated mockjax to latest version.
    
    Updated jquery to 1.5 and tested with tags and new mockjax

--- js/jquery.mockjax.js
+++ js/jquery.mockjax.js
@@ -1,12 +1,13 @@
 /*!
- * MockJax - Mock for Ajax requests
+ * MockJax - jQuery Plugin to Mock Ajax requests
  *
- * Version: 1.3
- * Released: 2010-07-21
- * Source: http://github.com/appendto/jquery-mockjax
- * Plugin: mockjax
- * Author: Jonathan Sharp (http://jdsharp.com)
- * License: MIT,GPL
+ * Version:  1.4.0
+ * Released: 2011-02-04
+ * Source:   http://github.com/appendto/jquery-mockjax
+ * Docs:     http://enterprisejquery.com/2010/07/mock-your-ajax-requests-with-mockjax-for-rapid-development
+ * Plugin:   mockjax
+ * Author:   Jonathan Sharp (http://jdsharp.com)
+ * License:  MIT,GPL
  * 
  * Copyright (c) 2010 appendTo LLC.
  * Dual licensed under the MIT or GPL licenses.
@@ -15,7 +16,36 @@
 (function($) {
 	var _ajax = $.ajax,
 		mockHandlers = [];
-
+	
+	function parseXML(xml) {
+		if ( window['DOMParser'] == undefined && window.ActiveXObject ) {
+			DOMParser = function() { };
+			DOMParser.prototype.parseFromString = function( xmlString ) {
+				var doc = new ActiveXObject('Microsoft.XMLDOM');
+		        doc.async = 'false';
+		        doc.loadXML( xmlString );
+				return doc;
+			};
+		}
+		
+		try {
+			var xmlDoc 	= ( new DOMParser() ).parseFromString( xml, 'text/xml' );
+			if ( $.isXMLDoc( xmlDoc ) ) {
+				var err = $('parsererror', xmlDoc);
+				if ( err.length == 1 ) {
+					throw('Error: ' + $(xmlDoc).text() );
+				}
+			} else {
+				throw('Unable to parse XML');
+			}
+		} catch( e ) {
+			var msg = ( e.name == undefined ? e : e.name + ': ' + e.message );
+			$(document).trigger('xmlParseError', [ msg ]);
+			return undefined;
+		}
+		return xmlDoc;
+	}
+	
 	$.extend({
 		ajax: function(origSettings) {
 			var s = jQuery.extend(true, {}, jQuery.ajaxSettings, origSettings),
@@ -49,52 +79,64 @@
 							 m = null;
 						}
 					}
-					// Inspect the data submitted in the request (either POST body or GET query string)
-					if ( m.data && s.data ) {
-						var identical = false;
-						// Deep inspect the identity of the objects
-						(function ident(mock, live) {
-							$.each(mock, function(k, v) {
-								if ( live[k] === undefined ) {
-									identical = false;
-									return false;
-								} else {
-									identical = true;
-									if ( typeof live[k] == 'object' ) {
-										return ident(mock[k], live[k]);
-									} else {
-										if ( $.isFunction( mock[k].test ) ) {
-											identical = mock[k].test(live[k]);
-										} else {
-											identical = ( mock[k] == live[k] );
-										}
-										return identical;
-									}
+					if ( m ) {
+						// Inspect the data submitted in the request (either POST body or GET query string)
+						if ( m.data && s.data ) {
+							var identical = false;
+							// Deep inspect the identity of the objects
+							(function ident(mock, live) {
+								// Test for situations where the data is a querystring (not an object)
+								if (typeof live === 'string') {
+									// Querystring may be a regex
+									identical = $.isFunction( mock.test ) ? mock.test(live) : mock == live;
+									return identical;
 								}
-								return true;
-							});
-						})(m.data, s.data);
-						// They're not identical, do not mock this request
-						if ( identical == false ) {
+								$.each(mock, function(k, v) {
+									if ( live[k] === undefined ) {
+										identical = false;
+										return false;
+									} else {
+										identical = true;
+										if ( typeof live[k] == 'object' ) {
+											return ident(mock[k], live[k]);
+										} else {
+											if ( $.isFunction( mock[k].test ) ) {
+												identical = mock[k].test(live[k]);
+											} else {
+												identical = ( mock[k] == live[k] );
+											}
+											return identical;
+										}
+									}
+								});
+							})(m.data, s.data);
+							// They're not identical, do not mock this request
+							if ( identical == false ) {
+								m = null;
+							}
+						}
+						// Inspect the request type
+						if ( m && m.type && m.type != s.type ) {
+							// The request type doesn't match (GET vs. POST)
 							m = null;
 						}
 					}
-					// Inspect the request type
-					if ( m.type && m.type != s.type ) {
-						// The request type doesn't match (GET vs. POST)
-						m = null;
-					}
 				}
 				if ( m ) {
-					if ( window.console && window.console.log ) {
-						window.console.log('MOCK GET: ' + s.url);
-					}
 					mock = true;
+
+					// Handle console logging
+					var c = $.extend({}, $.mockjaxSettings, m);
+					if ( c.log && $.isFunction(c.log) ) {
+						c.log('MOCK ' + s.type.toUpperCase() + ': ' + s.url, $.extend({}, s));
+					}
 					
+					var jsre = /=\?(&|$)/, jsc = (new Date()).getTime();
+
 					// Handle JSONP Parameter Callbacks, we need to replicate some of the jQuery core here
 					// because there isn't an easy hook for the cross domain script tag of jsonp
 					if ( s.dataType === "jsonp" ) {
-						if ( type === "GET" ) {
+						if ( s.type.toUpperCase() === "GET" ) {
 							if ( !jsre.test( s.url ) ) {
 								s.url += (rquery.test( s.url ) ? "&" : "?") + (s.jsonp || "callback") + "=?";
 							}
@@ -105,7 +147,6 @@
 					}
 			
 					// Build temporary JSONP function
-					var jsre = /=\?(&|$)/;
 					if ( s.dataType === "json" && (s.data && jsre.test(s.data) || jsre.test(s.url)) ) {
 						jsonp = s.jsonpCallback || ("jsonp" + jsc++);
 			
@@ -143,7 +184,7 @@
 						remote = parts && (parts[1] && parts[1] !== location.protocol || parts[2] !== location.host);
 					
 					// Test if we are going to create a script tag (if so, intercept & mock)
-					if ( s.dataType === "script" && s.type === "GET" && remote ) {
+					if ( s.dataType === "script" && s.type.toUpperCase() === "GET" && remote ) {
 						// Synthesize the mock request for adding a script tag
 						var callbackContext = origSettings && origSettings.context || s;
 						
@@ -180,65 +221,77 @@
 							(s.context ? jQuery(s.context) : jQuery.event).trigger(type, args);
 						}
 						
-						//if ( m.response && $.isFunction(m.response) ) {
-						//	m.response();
-						//} else {
+						if ( m.response && $.isFunction(m.response) ) {
+							m.response(origSettings);
+						} else {
 							$.globalEval(m.responseText);
-						//}
+						}
 						success();
 						complete();
 						return false;
 					}
-					_ajax.call($, $.extend(true, {}, origSettings, {
+					mock = _ajax.call($, $.extend(true, {}, origSettings, {
 						// Mock the XHR object
 						xhr: function() {
 							// Extend with our default mockjax settings
 							m = $.extend({}, $.mockjaxSettings, m);
+
+							if ( m.contentType ) {
+								m.headers['content-type'] = m.contentType;
+							}
+
 							// Return our mock xhr object
 							return {
 								status: m.status,
 								readyState: 1,
 								open: function() { },
 								send: function() {
-									var process = $.proxy(function() {
-										// The request has returned
-										this.status 		= m.status;
-										this.readyState 	= 4;
+									// This is a substitute for < 1.4 which lacks $.proxy
+									var process = (function(that) {
+										return function() {
+											return (function() {
+												// The request has returned
+											 	this.status 		= m.status;
+												this.readyState 	= 4;
 										
-										// We have an executable function, call it to give 
-										// the mock a chance to update it's data
-										if ( $.isFunction(m.response) ) {
-											m.response(s);
-										}
-										// Copy over our mock to our xhr object before passing control back to 
-										// jQuery's onreadystatechange callback
-										if ( s.dataType == 'json' && ( typeof m.responseText == 'object' ) ) {
-											this.responseText = JSON.stringify(m.responseText);
-										} else if ( s.dataType == 'xml' ) {
-											if ( $.xmlDOM && typeof m.responseXML == 'string' ) {
-												// Parse the XML from a string into a DOM
-												this.responseXML = $.xmlDOM( m.responseXML )[0];
-											} else {
-												this.responseXML = m.responseXML;
-											}
-										} else {
-											this.responseText = m.responseText;
-										}
-										this.onreadystatechange( m.isTimeout ? 'timeout' : undefined );
-									}, this);
-									
+												// We have an executable function, call it to give 
+												// the mock handler a chance to update it's data
+												if ( $.isFunction(m.response) ) {
+													m.response(origSettings);
+												}
+												// Copy over our mock to our xhr object before passing control back to 
+												// jQuery's onreadystatechange callback
+												if ( s.dataType == 'json' && ( typeof m.responseText == 'object' ) ) {
+													this.responseText = JSON.stringify(m.responseText);
+												} else if ( s.dataType == 'xml' ) {
+													if ( typeof m.responseXML == 'string' ) {
+														this.responseXML = parseXML(m.responseXML);
+													} else {
+														this.responseXML = m.responseXML;
+													}
+												} else {
+													this.responseText = m.responseText;
+												}
+												// jQuery < 1.4 doesn't have onreadystate change for xhr
+												if ( $.isFunction(this.onreadystatechange) ) {
+													this.onreadystatechange( m.isTimeout ? 'timeout' : undefined );
+												}
+											}).apply(that);
+										};
+									})(this);
+
 									if ( m.proxy ) {
 										// We're proxying this request and loading in an external file instead
 										_ajax({
 											global: false,
 											url: m.proxy,
-											type: m.type,
+											type: m.proxyType,
 											data: m.data,
 											dataType: s.dataType,
 											complete: function(xhr, txt) {
 												m.responseXML = xhr.responseXML;
 												m.responseText = xhr.responseText;
-												process();
+												this.responseTimer = setTimeout(process, m.responseTime || 0);
 											}
 										});
 									} else {
@@ -260,13 +313,20 @@
 									if ( m.headers && m.headers[header] ) {
 										// Return arbitrary headers
 										return m.headers[header];
-									} else if ( header == 'Last-modified' ) {
+									} else if ( header.toLowerCase() == 'last-modified' ) {
 										return m.lastModified || (new Date()).toString();
-									} else if ( header == 'Etag' ) {
+									} else if ( header.toLowerCase() == 'etag' ) {
 										return m.etag || '';
-									} else if ( header == 'content-type' ) {
+									} else if ( header.toLowerCase() == 'content-type' ) {
 										return m.contentType || 'text/plain';
 									}
+								},
+								getAllResponseHeaders: function() {
+									var headers = '';
+									$.each(m.headers, function(k, v) {
+										headers += k + ': ' + v + "\n";
+									});
+									return headers;
 								}
 							};
 						}
@@ -277,24 +337,30 @@
 			// We don't have a mock request, trigger a normal request
 			if ( !mock ) {
 				return _ajax.apply($, arguments);
+			} else {
+				return mock;
 			}
 		}
 	});
 
 	$.mockjaxSettings = {
-		//url: 			null,
-		//type: 			'GET',
-		status: 		200,
-		responseTime: 	500,
-		isTimeout:		false,
-		contentType: 	'text/plain',
-		response: 		'', 
-		responseText:	'',
-		responseXML:	'',
-		proxy:			'',
+		//url:        null,
+		//type:       'GET',
+		log:          function(msg) {
+		              	window['console'] && window.console.log && window.console.log(msg);
+		              },
+		status:       200,
+		responseTime: 500,
+		isTimeout:    false,
+		contentType:  'text/plain',
+		response:     '', 
+		responseText: '',
+		responseXML:  '',
+		proxy:        '',
+		proxyType:    'GET',
 		
-		lastModified:	null,
-		etag: 			'',
+		lastModified: null,
+		etag:         '',
 		headers: {
 			etag: 'IJF@H#@923uf8023hFO@I#H#',
 			'content-type' : 'text/plain'
